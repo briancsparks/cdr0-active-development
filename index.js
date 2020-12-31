b6fcb81f2289baea2588d7b08b0dfee3449edf64
Brian C Sparks <briancsparks@gmail.com>
2020-12-31 02:28:40 -0800

    Refactoring

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-const sg                      = require('./polyrepo/cdr0-sg');
+// const sg                      = require('./polyrepo/cdr0-sg');
 
 module.exports = function (that) {
   const dirname = that.path;
   // grumpy.if_(dirname === '', `module.path should not be empty`);
 
-  return sg.extend(
+  return Object.assign({},
     require('./lib/mode')(that),
   );
 };
